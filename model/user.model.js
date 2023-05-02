8d43beda55fce4f0b2fb421ff6f3bdab603534a3
utdsi <utkarshsinha852@gmail.com>
2023-05-02 12:04:54 +0530

    user model created

---
+++ model/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const {ObjectId} = mongoose.Schema.Types
+
+const userSchema = mongoose.Schema({
+    
+    name: String,
+    email: String,
+    password: String,
+    dob: Date,
+    bio: String,
+    posts: [{ type: ObjectId, ref: 'Post' }],
+    friends: [{ type: ObjectId, ref: 'User' }],
+    friendRequests: [{ type: ObjectId, ref: 'User' }]
+})
+
+
+const UserModel = mongoose.model("user",userSchema)
+
+module.exports = {
+    userSchema
+}
